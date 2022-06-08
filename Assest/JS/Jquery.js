0bee8a205404987ecae4ca354831ade69d26490a
raashid8 <94049778+raashid8@users.noreply.github.com>
2022-06-08 11:59:42 +0530

    ofc1
    first1

---
+++ Assest/JS/Jquery.js
@@ -0,0 +1,17 @@
+
+   $(document).ready(function () {
+    // Smooth Page Scroll to Top
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $(".scrollup").fadeIn();
+      } else {
+        $(".scrollup").fadeOut();
+      }
+    });
+
+    $(".scrollup").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 600);
+      return false;
+    });
+    // <!--//-->
+  });
